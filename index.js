58784696b2b614f3ec4907145d06b276cacd6cdd
tonnyopuch2023 <tonny.opuch@student.moringaschool.com>
2023-12-18 12:01:50 +0300

    done assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = 'Tonny Opuch';
+const welcomeMessage = 'Welcome to Flatbook, ${currentUser}!';
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreating = 'Welcome, ${currentUser.slice(0, 1)!';
